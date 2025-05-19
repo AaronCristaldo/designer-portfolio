2e8c79c23b36a5447427a0d027bf177566a13020
AaronCristaldo <aaroncristaldo5@gmail.com>
2025-05-19 03:05:43 +0200

    ssss

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/webApp/', // 👈 cambia esto
+  base: '/designer-portfolio/', // 👈 cambia esto
   plugins: [vue()]
 })
